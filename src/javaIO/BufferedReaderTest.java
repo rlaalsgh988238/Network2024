71e87c335362d9456ebcc4a27a9bd5e1453f3a5f
BOOK-119KMIQA3U\kmh03 <kmh030877@gmail.com>
2024-04-13 01:43:47 +0900

    javaIO finished

--- src/javaIO/BufferedReaderTest.java
+++ src/javaIO/BufferedReaderTest.java
@@ -1,13 +1,55 @@
 package javaIO;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.Reader;
 
 public class BufferedReaderTest{
     public static void main(String[] args) throws Exception{
         InputStreamReader isr = new InputStreamReader(System.in);
+        // InputStream은 Reader를 extend함.
+        // InputStream은 System.in에서 읽어들이고 BufferReader는 InputStream을 통해 읽어 들임
+
         BufferedReader br = new BufferedReader(isr);
         String line = br.readLine();
         System.out.println("키보드로부터 입력받은 문자열 :" + line);
     }
+}
+
+class SystemIn{
+    public static void main(String[] args) {
+        try {
+            while (true){
+                int d = System.in.read();
+                // -1 ~ 255
+
+                System.out.println(d);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+/**
+    s
+         115
+         10
+    sdfsdf
+         115
+         100
+         102
+         115
+         100
+         102
+         10
+    s
+            115
+         10
+    d
+         100
+         10
+    f
+         102
+         10
+*/
 }
--- src/javaIO/ChildTest.java
+++ src/javaIO/ChildTest.java
@@ -21,21 +21,23 @@ public class ChildTest {
     public static void main(String[] args) {
         Parent2 p = new Parent2();
         System.out.println("1번");
-        System.out.println(p.i);
-        System.out.println(p.get());
+        System.out.println(p.i); // 7
+        System.out.println(p.get()); // 7
 
         Child2 c = new Child2();
         System.out.println("2번");
-        System.out.println(c.i);
-        System.out.println(c.get());
+        System.out.println(c.i); // 5
+        System.out.println(c.get()); // 5
 
         Parent2 p2 = new Child2();
+        // new로 인해서 Child2는 힙에 올라가고 이를 참조하는 변수 형식은 Parent2임
+        // 참조변수가 Parent2면 필드는 Parent2를 사용. 오버라이딩 된 경우는 부모 메소드의 코드는 사라지고 자식 메소드의 코드만 남음
         System.out.println("3번");
-        System.out.println(p2.i);
-        System.out.println(p2.get());
+        System.out.println(p2.i); // 7 부모의 필드값
+        System.out.println(p2.get()); // 5 부모의 메소드를 오버라이딩한 자식의 메소드
 
         System.out.println("4번");
-        print(c);
+        print(c); //  7  5
         print(p2);
     }
 }
